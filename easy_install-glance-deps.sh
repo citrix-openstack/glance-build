2e6e10e1d0b451914270f3d24dec6c3c9e5a2f46
Ewan Mellor <ewan.mellor@citrix.com>
2011-12-18 18:09:34 -0800

    OS-864: Rebase Glance to Essex trunk (partial)
    Added pycrypto, which is now a dependency.

--- easy_install-glance-deps.sh
+++ easy_install-glance-deps.sh
@@ -5,6 +5,7 @@ set -ex
 MOCK="$1"
 
 # Dependencies for Glance
-$MOCK --chroot "easy_install-2.6 -vvv -H None -f /eggs -z \
+$MOCK --chroot "easy_install-2.6 -vvv -H None -f /eggs -z
                 argparse==1.1
+                pycrypto==2.4.1
 "
